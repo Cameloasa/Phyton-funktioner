580b829f9e7be9163fa5217e769d01e33289f536
Camelia Ciuca <camelia1414@gmail.com>
2025-01-30 11:56:07 +0100

    my_functions

--- main.py
+++ main.py
@@ -1,7 +1,24 @@
 from my_functions import (my_name,
                           eco,
-                          count_eco)
+                          count_eco,
+                          loop_five,
+                          last,
+                          cut_edges,
+                          increase,average,
+                          pretty_print)
 
-my_name("Cami")
-eco("Hej!")
-count_eco("Hello!", 3)
+my_name("Cami") # Output: Cami är en 🪽 på programmering!
+eco("Hej!") # Output: Hej! & Hej!
+count_eco("Hello!", 3) # Output: Hello!Hello!Hello!
+loop_five() # Output: 2, 4, 8, 16, 32
+last(lst= [1, 2, 3]) #Output: 3
+last(lst= []) #Output: Listan är tom!
+cut_edges(lst= [1, 2, 3, 4]) #Output: [2, 3]
+cut_edges(lst= [0, 1]) #Output: []
+cut_edges(lst= [1, 2, 3, 4, 5]) #Output: [2, 3, 4]
+print(increase(1)) #Output: 2
+print(average(2,4))
+pretty_print(lst= []) #Listan är tom, None
+pretty_print(lst= [1,-1, 3]) # Listan har 3 element:
+pretty_print(lst = [ "a", "b" ,3.14, "Cami"]) # Listan har 4 element:
+
--- my_functions.py
+++ my_functions.py
@@ -12,7 +12,6 @@ När den anropas med en sträng ska den upprepa strängen, och skriva ut resulta
 Exempel:
 eko("hej") → skriver ut "hejhej"
 """
-
 def eco(word):
     print(f"{word} & {word}")
 
@@ -22,18 +21,97 @@ som avgör hur många ekon som ska skapas.
 Exempel:
 eko("hej", 3) → skriver ut "hejhejhej"
 """
-
 def count_eco(word,count):
     result = word * count
     print(result)
     return result
 
 """
-3 Följande kod ska sluta loopa efter 5 varv. Flytta den in i en funktion och justera den enligt kommentaren.
+3 Följande kod ska sluta loopa efter 5 varv. 
+Flytta den in i en funktion och justera den enligt kommentaren.
 end = 5
 y = 1
 for x in range(1, 100):
     y *= 2
     # avsluta loopen med en if-sats här
 print(y)
+
 """
+def loop_five():
+    end = 5
+    y = 1
+    for x in range(1,100):
+        y *= 2
+        print(y)
+        if x == end:
+            break
+
+"""
+4 Skriv en funktion med namnet last. 
+Den ska ta en lista som parameter och returnera det sista elementet i listan.
+last([1, 2, 3]) → 3
+"""
+def last(lst):
+    if lst:  # Kollar om listan inte är tom
+        return print(lst[-1])  # Returnerar sista elementet
+    else:
+        return print("Listan är tom!")  # Returnerar None om listan är tom
+
+"""
+5 Skriv en funktion med namnet cut_edges. 
+Den ska ta en lista som parameter. 
+När den anropas ska den returnera en ny lista, 
+där den har tagit bort första och sista elementet.
+cut_edges([1, 2, 3, 4]) → [2, 3]
+"""
+def cut_edges(lst):
+    if len(lst) < 2:
+        return []
+    return print(lst[1:-1])
+
+"""
+6 Lös felen i koden.
+def increase(x):
+    return x
+    x += 1
+
+print(increase(1))
+"""
+def increase(x):
+    x += 1 # Öka värdet med 1
+    return x # Returnera det uppdaterade värdet
+
+"""
+7 Bygg en funktion med namnet average. 
+Den ska ta parametrar: x och y. 
+Båda ska vara tal. 
+Funktionen ska returnera medelvärdet. 
+Till exempel så räknar man ut medelvärdet av 4 och 8 genom med formeln: 
+(4 + 8) / 2, vilket blir 6.
+Tips: det kan vara enklare att använda fler variabler.
+"""
+def average(x,y):
+    total = x + y
+    avg = total / 2
+    return avg
+
+average_2 = lambda x, y: (x + y) / 2
+
+"""
+8 Gör en funktion som kan skriva ut innehållet i en lista lite snyggare. 
+Först ska funktionen tala om ifall listan är tom, eller hur många element den har. 
+Sedan ska funktionen skriva ut elementen i en punktlista. 
+Exempel:
+pretty_print(["a", "b", 3.14])
+Listan har 3 element:
+1. a
+2. b
+3. 3.14
+"""
+def pretty_print(lst):
+    if not lst:
+        print("Listan är tom.")
+    else:
+        print(f"Listan har {len(lst)} element:")
+        for i in range(len(lst)):
+            print(f"{i + 1}. {lst[i]}")
